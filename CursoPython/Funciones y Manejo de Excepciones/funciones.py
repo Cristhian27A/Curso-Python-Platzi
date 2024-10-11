d7dff4e1666213d34e0a9b09b659f88a7548c889
Cristhian27A <138025437+Cristhian27A@users.noreply.github.com>
2024-10-11 18:38:41 -0500

    Delete CursoPython directory

--- CursoPython/Funciones y Manejo de Excepciones/funciones.py
+++
@@ -1,6 +0,0 @@
-def greet(name, last_name="No tiene apellido"):
-    print("Hola", name, last_name)
-
-greet("Cristhian", "Alonso")
-greet("Julio")
-greet(last_name = "Alonso", name = "Cristhian")
--- CursoPython/control de flujo/ejercicio 3 iterador de cadenas.py
+++
@@ -1,9 +0,0 @@
-# Iteraremos en cadenas
-#se crean las cadena
-text = "Informatica teorica"
-#Creando el iterador
-iter_text = iter(text)
-
-#Iterar en la cadena
-for char in iter_text:
-    print(char)
